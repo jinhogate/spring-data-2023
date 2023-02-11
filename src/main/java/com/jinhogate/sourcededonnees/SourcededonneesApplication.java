4d2608ac12c4d2a56dc8360c1746e3467ab0c044
GBEGNON kossi <kossijorjinho.gbegnon@gmail.com>
2023-02-11 10:21:26 +0100

    Mise en place de la structure du projet

---
+++ src/main/java/com/jinhogate/sourcededonnees/SourcededonneesApplication.java
@@ -0,0 +1,13 @@
+package com.jinhogate.sourcededonnees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourcededonneesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourcededonneesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jinhogate/sourcededonnees/SourcededonneesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinhogate.sourcededonnees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourcededonneesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
