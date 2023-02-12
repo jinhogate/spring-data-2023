a460b806e813b83126ac572e45a3a4e96ad9623e
GBEGNON kossi <kossijorjinho.gbegnon@gmail.com>
2023-02-12 23:57:13 +0100

    Création d'une instance de log pour afficher les valeurs de myBean dans la console

--- src/main/java/com/jinhogate/sourcededonnees/SourcededonneesApplication.java
+++ src/main/java/com/jinhogate/sourcededonnees/SourcededonneesApplication.java
@@ -1,13 +1,31 @@
 package com.jinhogate.sourcededonnees;
 
+import java.util.logging.Logger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.jinhogate.sourcededonnees.frameworktest.MyBean;
+
 @SpringBootApplication
 public class SourcededonneesApplication {
+	
+	private static Logger log = Logger.getLogger(SourcededonneesApplication.class.getName());
+	
+	private static MyBean myBean;
+	
+	public SourcededonneesApplication(MyBean myBean) {
+		this.myBean = myBean;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SourcededonneesApplication.class, args);
+//		log.info("Propriété URL ----------> " + myBean.getUrl());
+//		log.info("Propriété information system (user name)  -----> " + myBean.getInformationSystem());
+//		log.info("Propriété de ma bean --------> " + myBean.getMyBeanProp());
+//		log.info("Nom de mon application  ------------> " + myBean.getApplicationName());
+//		log.info("les arguments  ------------> " + args.length);
+		log.info("le mot de passe   ------------> " + myBean.getPassWord());
 	}
 
 }
